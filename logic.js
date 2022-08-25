ec7e1882c2fa055f0c9d6ce1d5a8d436a115f93d
deeparka <arkadeep180@gmail.com>
2022-08-25 14:22:25 +0530

    changed the ui color

--- logic.js
+++ logic.js
@@ -6,59 +6,60 @@ let play = false;
 let newWords = "";
 let ranWords = "";
 let sWords = [
-  "pasta",
-  "committee",
-  "functional",
-  "java",
-  "python",
-  "horizontal",
-  "vertical"
+    "pasta",
+    "committee",
+    "function",
+    "java",
+    "python",
+    "horizontal",
+    "vertical",
+    "pizza",
 ];
 
 const createNewWords = () => {
-  let ranNum = Math.floor(Math.random() * sWords.length);
-  //console.log(ranMum);
-  let newTempWords = sWords[ranNum];
-  //console.log(newTempWords);
-  return newTempWords;
+    let ranNum = Math.floor(Math.random() * sWords.length);
+    //console.log(ranMum);
+    let newTempWords = sWords[ranNum];
+    //console.log(newTempWords);
+    return newTempWords;
 };
 
 const scrambleWords = (arr) => {
-  for (let i = arr.length - 1; i >= 0; i--) {
-    let element = arr[i];
-    //console.log(element);
-    let j = Math.floor(Math.random() * (i + 1));
-    //console.log(i);
-    //console.log(j);
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let element = arr[i];
+        //console.log(element);
+        let j = Math.floor(Math.random() * (i + 1));
+        //console.log(i);
+        //console.log(j);
 
-    arr[i] = arr[j];
-    arr[j] = element;
-  }
-  return arr;
+        arr[i] = arr[j];
+        arr[j] = element;
+    }
+    return arr;
 };
 
 btn.addEventListener("click", () => {
-  if (!play) {
-    play = true;
-    btn.innerHTML = "Guess";
-    guess.classList.toggle("hidden");
-    newWords = createNewWords();
-    randWords = scrambleWords(newWords.split("")).join("");
-    //console.log(randWords.join(""));
-    msg.innerHTML = `Guess the word: ${randWords}`;
-  } else {
-    let tempWord = guess.value;
-    if (tempWord === newWords) {
-      //console.log("correct");
-      play = false;
-      msg.innerHTML = `Yay! you are right. It is \"${newWords}"`;
-      btn.innerHTML = "Guess next word";
-      guess.classList.toggle("hidden");
-      guess.value = "";
+    if (!play) {
+        play = true;
+        btn.innerHTML = "Guess";
+        guess.classList.toggle("hidden");
+        newWords = createNewWords();
+        randWords = scrambleWords(newWords.split("")).join("");
+        //console.log(randWords.join(""));
+        msg.innerHTML = `Guess the word: ${randWords}`;
     } else {
-      //console.log("incorrect");
-      msg.innerHTML = `Oops! you are wrong. Try again. \"${randWords}"`;
-      guess.value = "";
+        let tempWord = guess.value;
+        if (tempWord === newWords) {
+            //console.log("correct");
+            play = false;
+            msg.innerHTML = `Yay! you are right. It is \"${newWords}"`;
+            btn.innerHTML = "Guess next word";
+            guess.classList.toggle("hidden");
+            guess.value = "";
+        } else {
+            //console.log("incorrect");
+            msg.innerHTML = `Oops! you are wrong. Try again. \"${randWords}"`;
+            guess.value = "";
+        }
     }
-  }
 });
